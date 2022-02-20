70965bb41c4f4f7b155be933706ec44456e8638a
Alice Ding <dingjuhz@gmail.com>
2022-02-20 19:12:44 +0800

    inital check in

---
+++ DesignPatterns/DesignPatterns/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DesignPatterns
+//
+//  Created by Alice on 20/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
